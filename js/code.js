a5fde6bebd4865037996a10f9b46e167c50f7dc1
Lawrence <lawrence.ora@mail.utoronto.ca>
2020-05-13 21:26:23 +0800

    removed jquery, added vanila js

--- js/code.js
+++ js/code.js
@@ -8,8 +8,46 @@ var fullPage = new fullpage('#fullpage', {
     navigationTooltips: ['Home', 'About', 'Work', 'Contact']
 });
 
-$(document).ready(function(){
-    $('#horizontal-scroll').on("swipe", function(){
-        alert("Swipe Detected");
-    });
-});
+// All this JS for overriding scroll for mobile.
+document.getElementById("horizontal-scroll").addEventListener('touchstart', handleTouchStart, false);        
+document.getElementById("horizontal-scroll").addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) < Math.abs(yDiff)){
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            fullpage_api.moveSectionUp();
+        } else { 
+            /* down swipe */
+            fullpage_api.moveSectionDown();
+        }                         
+    } // else here, check xDiff for right and left swipes.
+                                                
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};

9577a448811369d6237b0d394dfc7b0dbaa3a390
Lawrence <lawrence.ora@mail.utoronto.ca>
2020-05-15 01:24:34 +0800

    trying out carousel implementation

--- js/code.js
+++ js/code.js
@@ -3,51 +3,50 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: false,
     verticalCentered: false,
-    normalScrollElements: '#horizontal-scroll',
     anchors: ['Home', 'About', 'Work', 'Contact'],
     navigationTooltips: ['Home', 'About', 'Work', 'Contact']
 });
 
 // Vanilla JS for overriding scroll for mobile.
-document.getElementById("horizontal-scroll").addEventListener('touchstart', handleTouchStart, false);        
-document.getElementById("horizontal-scroll").addEventListener('touchmove', handleTouchMove, false);
+// document.getElementById("horizontal-scroll").addEventListener('touchstart', handleTouchStart, false);        
+// document.getElementById("horizontal-scroll").addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
-var yDown = null;
+// var xDown = null;                                                        
+// var yDown = null;
 
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+// function getTouches(evt) {
+//   return evt.touches ||             // browser API
+//          evt.originalEvent.touches; // jQuery
+// }                                                     
 
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+// function handleTouchStart(evt) {
+//     const firstTouch = getTouches(evt)[0];                                      
+//     xDown = firstTouch.clientX;                                      
+//     yDown = firstTouch.clientY;                                      
+// };                                                
 
-function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-        return;
-    }
+// function handleTouchMove(evt) {
+//     if (!xDown || !yDown) {
+//         return;
+//     }
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
+//     var xUp = evt.touches[0].clientX;                                    
+//     var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
 
-    if (Math.abs(xDiff) < Math.abs(yDiff)){ 
-        if (yDiff > 0) {
-            /* up swipe */     
-            fullpage_api.moveSectionDown();
-        } else { 
-            /* down swipe */
-            fullpage_api.moveSectionUp();
-        }                         
-    } // else here, check xDiff for right and left swipes.
+//     if (Math.abs(xDiff) < Math.abs(yDiff)){ 
+//         if (yDiff > 0) {
+//             /* up swipe */     
+//             fullpage_api.moveSectionDown();
+//         } else { 
+//             /* down swipe */
+//             fullpage_api.moveSectionUp();
+//         }                         
+//     } // else here, check xDiff for right and left swipes.
                                                 
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;                                             
+// };

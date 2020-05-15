8a5bc9603dbb7772e41909d505a6d78fb2ae93b3
Lawrence <lawrence.ora@mail.utoronto.ca>
2020-05-15 17:19:10 +0800

    desktop build done, plus clean up

--- js/code.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#fullpage', {
-    fitToSection: false,
-    autoScrolling: true,
-    navigation: false,
-    verticalCentered: false,
-    anchors: ['Home', 'About', 'Work', 'Contact'],
-    navigationTooltips: ['Home', 'About', 'Work', 'Contact']
-});
